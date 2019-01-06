b8dc6df812cddd823b36d047b08b4bf3548b84e6
谷口洋斗 <gucci.taniguchi88@gmail.com>
2019-01-06 22:27:49 +0900

    if文

--- study.cpp
+++ study.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 int main ()
 {
-  cout << "Hello!" << endl;
-  return 0;
+  int x;
+  cout << "年齢を入力してください:" ;
+  cin >> x;
+  cout << "あなたは" << x << "歳なんですね。" << endl;
+  if (x >= 20){
+    cout << "もっと若く見えますよ" << endl;
+  } else {
+    cout << "意外と若いんですね。" << endl;
+  }
 }
