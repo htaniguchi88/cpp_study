9fb06d05e2621263a36f19fe869662dc924c1c1c
谷口洋斗 <gucci.taniguchi88@gmail.com>
2019-01-06 22:36:34 +0900

    string型

--- study.cpp
+++ study.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
 
 int main ()
 {
-  int x;
-  cout << "年齢を入力してください:" ;
-  cin >> x;
-  cout << "あなたは" << x << "歳なんですね。" << endl;
-  if (x >= 20){
+  string name;
+  int age;
+  cout << "名前を入力して下さい:" ;
+  cin >> name;
+  cout << "年齢を入力して下さい:" ;
+  cin >> age;
+
+  cout << name << "さんは" << age << "歳なんですね。" << endl;
+  if (age >= 20){
     cout << "もっと若く見えますよ" << endl;
   } else {
     cout << "意外と若いんですね。" << endl;
