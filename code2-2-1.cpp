3d6d296e231c5630f840362b0291916f85b06f34
谷口洋斗 <gucci.taniguchi88@gmail.com>
2019-01-08 10:13:19 +0900

    add 2-2-1file

---
+++ code2-2-1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class KNeko
+{
+  string name;
+public:
+  void SetName(string n){
+    name = n;
+  }
+  void Naku() const{
+    cout << "にゃー。俺様は" << name << "だ。" << endl;
+  }
+};
+
+int main()
+{
+  KNeko x;
+  x.Naku();
+
+  x.SetName("タマ");
+  x.Naku();
+}
