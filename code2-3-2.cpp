1841babbb46a4b18106b0703ae9f9a6e965cabe2
谷口洋斗 <gucci.taniguchi88@gmail.com>
2019-01-08 22:58:21 +0900

    code2-3-2 make objects

---
+++ code2-3-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class KNeko
+{
+  string name;
+public:
+  KNeko(string n){
+    name = n;
+  }
+  void SetName(string n){
+    name = n;
+  }
+  void Naku() const{
+    cout << "にゃー。俺様は" << name << "だ。" << endl;
+  }
+};
+
+int main()
+{
+  KNeko x("たろうちゃま");
+  x.Naku();
+  KNeko y("ひーくんちゃま");
+  y.Naku();
+  KNeko z("yuritani");
+  z.Naku();
+}
