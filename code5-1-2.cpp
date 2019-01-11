190823f77e357c9d1ab3fd82d2867a84abf0f622
谷口洋斗 <gucci.taniguchi88@gmail.com>
2019-01-11 10:32:01 +0900

    code5-1-2

---
+++ code5-1-2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main()
+{
+  srand((usigned)time(NULL));
+  cout << rand() << endl;
+  cout << rand() << endl;
+  cout << rand() << endl;
+}
