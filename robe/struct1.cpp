fd106309dc2a4fd6fdf247da095570e0252b512f
谷口洋斗 <gucci.taniguchi88@gmail.com>
2019-04-27 15:23:43 +0900

    Make rove derectory

---
+++ robe/struct1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_NAME = 16;
+
+struct Student {
+    char name[MAX_NAME + 1];
+    int scoreJapanese;
+    int scoreMath;
+    int scoreEnglish;
+};
+
+void Show(const Stundent& student) //Stundentへの参照を引数にとる関数show
+{
+    cout << "名前"
+}
