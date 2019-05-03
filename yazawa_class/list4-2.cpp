4fe3b7c9c0f83ead8be194d4dcc857922a5ccdb9
谷口洋斗 <gucci.taniguchi88@gmail.com>
2019-05-03 16:27:10 +0900

    Fix bug

--- yazawa_class/list4-2.cpp
+++ yazawa_class/list4-2.cpp
@@ -26,6 +26,10 @@ bool SimpleMath::quadratic(double a, double b, double c, double *x1, double *x2)
 	}
 }
 
+double SimpleMath::tempCalc(double a, double b, double c) {
+	return b * b - 4 * a * c;
+}
+
 int main() {
 	SimpleMath sm;
 	double x1, x2;
