f17eddbbc710cc944ed011c298ce8092e1c7a797
谷口洋斗 <gucci.taniguchi88@gmail.com>
2019-05-13 00:37:43 +0900

    ポインタの復習

--- class_review/20190512/main.cpp
+++ class_review/20190512/main.cpp
@@ -11,12 +11,25 @@ void showInt(int x) {
 	cout << "入力した数字は" << x << endl;
 }
 
+void rewrite_number (int* to_rewrite_number) {
+	*to_rewrite_number = 1000;
+}
+
 int main() {
 	int input_number;
+	int number;
+	int* to_rewrite_number;
 
-	cout << "数字を入力して下さい" << endl;
-	cin >> input_number;
+	number = 1;
+	to_rewrite_number = &number;
 
-	showInt(input_number);
+	// cout << "数字を入力して下さい" << endl;
+	// cin >> input_number;
+
+	// showInt(input_number);
+
+	rewrite_number(to_rewrite_number);
+
+	cout << *to_rewrite_number << endl;
 
 }
