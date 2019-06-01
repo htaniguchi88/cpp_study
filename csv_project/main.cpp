8a925ae97807c55c0b9bae8211cd5a548d42e9dc
谷口洋斗 <gucci.taniguchi88@gmail.com>
2019-06-01 12:14:38 +0900

    C++ - Reading .csv file into vectors...の記事を読んで理解中

---
+++ csv_project/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Member {
+    int32_t group_id;
+    char name[7];
+    unsigned char id;
+    int32_t score;
+};
+
+int main(){
+	vector<Member> members;
+
+	ifstream ifs("day3_q3.csv");
+	if(!ifs){
+		cerr << "ファイルオープンに失敗" << endl;
+		exit(1);
+	}
+
+	string str;
+	getline(ifs, str);
+
+	while(getline(ifs, str)){
+		Member member;
+		string token;
+		// getlineで文字が流し込まれたstrをistringstreamのオブジェクトであるissのメンバに渡すことで，もう一度ストリームに戻している。istringstreamの中では引数を受け取る変数初期化のコンストラクタがあるのだろう。
+		istringstream iss(str);
+		string token;
+
+		getline(iss, member.group_id, ',');
+
+	}
+
+
+
+}
